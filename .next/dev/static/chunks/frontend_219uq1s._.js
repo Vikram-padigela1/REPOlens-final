3be482147181a7164f98346790cb5f6e4598c425
Vikram-padigela1 <saradhi.padigela8@gmail.com>
2026-09-15 13:51:04 +0530

    chore: purge legacy build chunks, untrack venv, and update .gitignore

--- .next/dev/static/chunks/frontend_219uq1s._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__0c7uqo1._.css","static/chunks/19xk_next_dist_0r0fjwb._.js","static/chunks/frontend_src_0chlccg._.js","static/chunks/19xk_@xyflow_system_dist_esm_index_09226p1.js","static/chunks/19xk_@xyflow_react_dist_esm_index_1w24on-.js","static/chunks/19xk_micromark-core-commonmark_dev_lib_1pf3ljh._.js","static/chunks/19xk_14mhs9j._.js","static/chunks/19xk_@xyflow_react_dist_style_1hsueuw.css"],
-    source: "entry"
-});
